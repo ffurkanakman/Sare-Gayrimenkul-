f9edead7d1b680a6db9c755eedc52c52259a1fc9
ffurkan <akman.57.furkan@gmail.com>
2025-08-20 15:21:53 +0300

    bug fix

--- app/Modules/User/Http/Resources/UserResource.php
+++ app/Modules/User/Http/Resources/UserResource.php
@@ -9,6 +9,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'  => $this->id,
             'name' => $this->name,
             'surname' => $this->surname,
             'phone_number' => $this->phone_number,
---
+++ resources/js/Pages/Ui/Customers/AccountSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccountSettings = () => {
+    return (
+        <div className="container py-4">
+            {/* boş sayfa */}
+        </div>
+    );
+};
+
+export default AccountSettings;
